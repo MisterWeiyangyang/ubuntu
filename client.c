c03407bb803ed750121c12f549af8eb1d4f91b18
weiyangyang <2746740439@qq.com>
2023-03-30 20:33:37 +0800

    server_clinet_tcp

---
+++ client.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 12345
+
+int main(int argc, char const *argv[])
+{
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+       
+    // 将IP地址从字符串转换为二进制格式
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+    // 连接服务器
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    // 发送消息到服务器
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    // 读取服务器发送的消息
+    valread = read( sock , buffer, 1024);
+    printf("%s\n",buffer );
+    // 关闭socket连接
+    close(sock);
+    return 0;
+}
+
